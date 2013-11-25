a26afc9d293881f38b0d910d923b1a22cb7d1149
Tom Sercu <tom.sercu@gmail.com>
2013-11-24 23:55:53 -0500

    Testing locally

--- select_frames.py
+++ select_frames.py
@@ -12,15 +12,26 @@ class Selector:
     def __init__(self,path):
         #path is folder with frames and info.
         assert(isdir(path) and isfile(join(path,'info.pk')))
+        print "Opening selector for %s"%path
         self.path=path
         self.loadpickle()
+        # determine framesize
+        img=cv2.imread(self.shots_info[0][0][2])
+        x,y,c=img.shape
+        assert(c==3)
+        self.x=x
+        self.y=y
+        print "Frames are {0}x{1}".format(x,y)
         # Load images
+        print "Load images",
         self.shots=[]
-        for i,shot in enumerate(self.shots_info):
-            self.shots.append([])
-            for j,frame in enumerate(shot):
+        for si,shot in enumerate(self.shots_info):
+            nframes=len(shot)
+            self.shots.append(np.zeros((nframes,x,y,3))) # allocate for speed
+            for fi,frame in enumerate(shot):
                 img=cv2.imread(frame[2])
-                self.shots[i].append(img)
+                self.shots[si][fi,:,:,:]=img
+                print ".",
 
 
     def loadpickle(self):
@@ -28,8 +39,29 @@ class Selector:
         self.headers_info=tmp['headers']
         self.shots_info=tmp['shots']
 
-    def showimg(self,sid,fid):
-        pass
+    def showimg(self,i,j):
+        plt.imshow(self.shots[i][j]);
+        plt.show()
+
+    def showgrid(self,inds={}):
+        """ inds contains dict with key: shotid, vals: frameids. """
+        rows=len(inds)
+        cols=max( [len(inds[k]) for k in inds])
+        i=0
+        plt.figure(figsize=(20,12))
+        for si in enumerate(inds):
+            print ('shot %d:'%si),
+            for fi in inds[si]:
+                fr=self.shots[si][fi]
+                i+=1
+                plt.subplot(rows,cols,i)
+                plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01)
+                print ('%d,'%fr),
+                plt.imshow(self.d[fr])
+                plt.axis('off')
+            i=cols*r
+            print('')
+        plt.show()
 
     def filter_shotlength(self):
         pass
@@ -44,5 +76,5 @@ class Selector:
         pass
 
 if __name__=="__main__":
-    path='/home/ts2387/frames/Bears/txED4ByjtCU/'
+    path='/home/tom/frames/Bears/EgcVTNgOkV4/'
     sel=Selector(path)
