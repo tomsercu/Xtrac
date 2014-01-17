770516a71098cdf49d4868971d19d726727b0a2a
Tom Sercu <tom.sercu@gmail.com>
2014-01-17 15:52:18 -0500

    Make download modular and based on conf.py

--- conf.py
+++ conf.py
@@ -1,11 +1,13 @@
 
 from os.path import join, expanduser
+import datetime
 
 ### PATHS
 class path:
-    video = expanduser('~/youtube ')
+    video = expanduser('~/youtube')
     thumbs = expanduser('~/thumbs')   # sparse small thumbs -- small part of frames
     frames = expanduser('~/frames')   # frames for tracking -- all frames
+    track  = '../region_proposals'
     output = expanduser('~/cifar32')  # actual selections
 
 ### DOWNLOADS
@@ -13,6 +15,8 @@ class down:
     Npersubj = 50 # number of downloads per subject
     maxpages = 5  # number of pages per query
     queryfile = 'cifar10.json'
+    ytstring = 'http://www.youtube.com/results?filters=video%2C+long&'
+    ids_fn = join(path.video, 'down_ids_%s.json' % datetime.datetime.now().strftime('%Y-%m-%d_%Hh%M'))
 
 ### FFMPEG PROCESSING AND SHOT DETECTION
 class shotdetect:
@@ -36,9 +40,11 @@ class filter:
     min_dark_area  = 0.15
 
 ### TRACKING
+matlab = 'optirun /usr/local/bin/matlab -nodesktop -nosplash'
 class tracking:
     w = 100
     h = -1
     class fast:
         min = 50
         num = 20
+
--- download.py
+++ download.py
@@ -8,97 +8,106 @@ import shutil
 import cPickle as pickle
 import datetime
 import simplejson as json
+import conf
 
 # TODO detect doubles in selected queries!
-#===============================================================================
-# Parameters
-#===============================================================================
-outdir=expanduser('~/youtube')
-Npersubj=50 # increase to download more per subject
-maxpages=5
-ytstring='http://www.youtube.com/results?filters=video%2C+long&'
-#===============================================================================
-# Load all queries
-#===============================================================================
-with open('cifar10.json','rU') as fh:
-    queries=json.load(fh)
-subjects=queries.keys()
 
 #===============================================================================
 # What did we already downlod
 #===============================================================================
 movielist = set(); # movies downloaded over all subdirs
+
 def updatemovielist():
-    global movielist, outdir
-    for subj in listdir(outdir):
-        spath=join(outdir,subj)
+    global movielist
+    for subj in listdir(conf.path.video):
+        spath=join(conf.path.video, subj)
         if isdir(spath):
             for f in listdir(spath):
                 if isfile(join(spath, f)):
                     if f[-4:]=='.mp4' or f[-4:]=='.flv':
-                        movielist.add(f[0:-4])
-#===============================================================================
-# Generate directories if needed
-#===============================================================================
-for subj in subjects:
-    spath=join(outdir,subj)
-    if not os.path.exists(spath):
-        print "Make dir %s"%spath
-        os.mkdir(spath)
+                        vidid = f[0:-4]
+                        movielist.add(vidid)
+
+def get_video_ids(write = True):
+    global movielist
+    #===============================================================================
+    # Load all queries
+    #===============================================================================
+    with open(conf.down.queryfile,'rU') as fh:
+        queries=json.load(fh)
+    subjects=queries.keys()
+
+    #===============================================================================
+    # Assemble videos to download, including movies we already have.
+    #===============================================================================
+    down_ids={}
+    for subj in subjects:
+        Nperquery=conf.down.Npersubj/len(queries[subj])
+        down_ids[subj]={}
+        print "Looking for subject %s"%subj
+        for query in queries[subj]:
+            down_ids[subj][query]=[]
+            enough=False
+            p=0
+            while (not enough and p < conf.down.maxpages): # open new pages
+                p+=1
+                print "Subject %s, query:  %s page %d, found %d movies"%(subj,query,p, Nperquery)
+                url = conf.down.ytstring+'search_query=%s&page=%d' % (query.replace(' ','+'), p)
+                response = urllib2.urlopen(url)
+        #         response = urllib2.urlopen('http://www.youtube.com/results?search_query=discovery%20channel&page=' + str(i))
+                html = response.read()
+                for m in re.finditer('watch\?v=', html):
+                    tmp = html[(m.end() - 1):]
+                    endidx = tmp.find('"')
+                    if endidx > 15:
+                        continue
+                    vidid= tmp[1:endidx]
+                    down_ids[subj][query].append(vidid)
+                    if (len(down_ids[subj][query]) == Nperquery):
+                        enough=True
+                        break
+    if write:
+        # Write video id download list
+        json.dump(down_ids, open(conf.down.ids_fn, 'w'), indent = 2, sort_keys = True)
+    return down_ids
+
+def download(down_ids):
+    global movielist
+    """ Use subprocess youtube-dl to download videos.
+    expects down_ids to be a dictionary with subjects/categories as keys,
+    per subject a dictionary with quries as keys,
+    per query a list of video ids."""
+
+    #===============================================================================
+    # Make directories if needed
+    #===============================================================================
+    subjects = down_ids.keys()
+    for subj in subjects:
+        spath=join(conf.path.video, subj)
+        if not os.path.exists(spath):
+            print "Make dir %s"%spath
+            os.mkdir(spath)
+
+    #===============================================================================
+    # Download videos
+    #===============================================================================
+    for subj in subjects:
+        queries = down_ids[subj].keys()
+        for q in queries:
+            for vidid in down_ids[subj][q]:
+                updatemovielist() # account for parallel execution
+                if (vidid not in movielist):
+                    movielist.add(vidid)
+                    out=join(conf.path.video, subj, vidid+'.mp4')
+                    cli = 'youtube-dl -o %s --write-description --all-subs --write-info-json http://www.youtube.com/watch?v=%s'%(out, vidid)
+                    print '%s -- download:  %s ' % (vidid, cli)
+                    subprocess.call(cli, shell=True)
+                else:
+                    print "%s -- Movie  is already downloaded, skip" % vidid
 
 
-#===============================================================================
-# Assemble videos to download, including movies we already have.
-#===============================================================================
-dlist={}
-for subj in subjects:
-    Nperquery=Npersubj/len(queries[subj])
-    dlist[subj]={}
-    print "Looking for subject %s"%subj
-    for query in queries[subj]:
-        dlist[subj][query]=[]
-        enough=False
-        p=0
-        while (not enough and p<maxpages): # open new pages
-            p+=1
-            print "Subject %s, query:  %s page %d, downloading %d movies"%(subj,query,p, Nperquery)
-            url=ytstring+'search_query=%s&page=%d' % (query.replace(' ','+'), p)
-            response = urllib2.urlopen(url)
-    #         response = urllib2.urlopen('http://www.youtube.com/results?search_query=discovery%20channel&page=' + str(i))
-            html = response.read()
-            for m in re.finditer('watch\?v=', html):
-                tmp = html[(m.end() - 1):]
-                endidx = tmp.find('"')
-                if endidx > 15:
-                    continue
-                movie = tmp[1:endidx]
-                dlist[subj][query].append(movie)
-                if (len(dlist[subj][query]) == Nperquery):
-                    enough=True
-                    break
-
-#===============================================================================
-# Dump current download list for future reference (what queries suck)
-#===============================================================================
-now=datetime.datetime.now()
-fn='dlist_%s.dlp' % now.strftime('%Y-%m-%d_%Hh%M')
-pickle.dump(dlist, open(join(outdir,fn), 'wb'))
-#===============================================================================
-# Download videos
-#===============================================================================
-for subj in subjects:
-    for q in queries[subj]:
-        for movie in dlist[subj][q]:
-            updatemovielist()
-            if (movie not in movielist):
-                movielist.add(movie)
-                out=join(outdir,subj,movie+'.mp4')
-                cli = 'youtube-dl -o %s --write-description --all-subs --write-info-json http://www.youtube.com/watch?v=%s'%(out,movie)
-                print cli
-                subprocess.call(cli, shell=True)
-            else:
-                print "%s -- Movie  is already downloaded, skip"%movie
-
-
+if __name__ == "__main__":
+    down_ids = get_video_ids()
+    download(down_ids)
 
 
