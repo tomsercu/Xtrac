d3780847d8e14c9c44065f9c37a649d7b1d364b3
Tom Sercu <tom.sercu@gmail.com>
2014-01-17 10:05:42 -0500

    Show grid with different filter results

--- select_cheap.py
+++ select_cheap.py
@@ -16,6 +16,7 @@ import cPickle as pickle
 import datetime
 import simplejson as json
 import glob
+from util import full_extent
 
 #=================
 #FILTER PARAMETERS
@@ -23,18 +24,28 @@ import glob
 MIN_SHOT_LEN=3
 MIN_REL_DIFF_FRAMES=0.02
 MAX_REL_DIFF_FRAMES=0.4
-MIN_BRIGHTNESS=10
-MAX_BRIGHTNESS=200
+MIN_BRIGHTNESS=47
+MAX_BRIGHTNESS=185
 
 #FILTER IDS
 #==========
 filter_ids={
+'pass':1,
 'short':-1,
 'bright':-2,
 'dark':-3,
 'static':-4,
 'dynamic':-5,
 'unnatural':-6,
+}
+filter_colors={
+'pass':'g',
+'short':'r',
+'bright':'y',
+'dark':'k',
+'static':'b',
+'dynamic':'r',
+'unnatural':'c',
 }
 
 #=================
@@ -67,14 +78,14 @@ class Selector:
             self.shots_pass=np.ones((self.Nshots))
             self.filter_ids=filter_ids
 
-    def loadimages(self):
+    def loadimages(self, all=False):
         if len(self.shots)==self.Nshots:
             return False
         # Load images
         print "{0} - Load all shots".format(self.vidid)
         self.shots=[]
         for si,shot in enumerate(self.shots_info):
-            if self.shots_pass[si]<=0:
+            if self.shots_pass[si]<=0 and not all:
                 self.shots.append(None)
                 continue
             nframes=len(shot)
@@ -85,7 +96,6 @@ class Selector:
             #print ".",
         #print ""
         return True
-
 
     def loadpickle(self):
         self.shots_info=pickle.load(open(join(self.path,'info.pk'),'rb'))
@@ -111,6 +121,57 @@ class Selector:
         plt.imshow(self.shots[i][j])
         plt.show()
 
+    def showgrid(self, frames, tile=(20,30),colors=None):
+        assert(len(frames)==tile[0]*tile[1])
+        rows=tile[0]
+        cols=tile[1]
+        fig=plt.figure(figsize=(18,12))
+        #fig, axes = plt.subplots(rows,cols)
+        #fig.set_size_inches((18,12))
+        # http://stackoverflow.com/questions/12439588/how-to-maximize-a-plt-show-window-using-python
+        #figManager = plt.get_current_fig_manager()
+        #figManager.window.showMaximized()
+        axes=[]
+        for i,fr_id in enumerate(frames):
+            if type(fr_id)==np.ndarray:
+                fr=fr_id
+            elif type(fr_id)==tuple: # shot id, frame id
+                fr=self.shots[fr_id[0]][fr_id[1]]
+            else:
+                fr=None
+                axes.append(None)
+                continue
+            ax=plt.subplot(rows,cols,i+1)
+            axes.append(ax)
+            plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01)
+            plt.imshow(fr)
+            plt.axis('off')
+            if i%cols==0:
+                print ""
+            print ('%d'%i),
+        print ""
+
+        if colors is not None:
+            print "Coloring background"
+            from matplotlib.transforms import Bbox
+            from matplotlib.patches import Rectangle
+            #axes=axes.flatten()
+            colors.insert(0,(0,'')) # add starting point for rectangles
+            for ci in range(1,len(colors)):
+                istart = colors[ci-1][0]
+                iend   = colors[ci][0]
+                extent = Bbox.union([full_extent(ax) for ax in axes[istart:iend] if ax is not None])
+                #extent=Bbox.union(axes[istart:iend])
+                extent = extent.transformed(fig.transFigure.inverted())
+                # We can now make the rectangle in figure coords using the "transform" kwarg.
+                rect = Rectangle([extent.xmin, extent.ymin], extent.width, extent.height,
+                                         facecolor=colors[ci][1] ,edgecolor='none', zorder=-1,
+                                                          transform=fig.transFigure)
+                fig.patches.append(rect)
+        print "Save and show"
+        plt.savefig('grid.png')
+        plt.show()
+
     def showshots(self,inds={}):
         """ inds contains dict with key: shotid, vals: frameids. """
         rows=len(inds)
@@ -133,8 +194,39 @@ class Selector:
             print('')
         plt.show()
 
-    def show_filter_sample(self,filter_id):
-        pass
+    def show_filter_sample(self,filter_show=['pass'], tile=(16,21),frames_per_shot=3):
+        """Show a grid of shots with filter codes in filter_show,
+        and number of frames in tile."""
+        self.loadimages(all=True)
+        #Nframes=tile[0]*tile[1]
+        rows_per_filter=[tile[0]/len(filter_show)]*len(filter_show)
+        rows_per_filter[0]=tile[0]-np.sum(rows_per_filter[1:])
+        Nframes=np.cumsum(rows_per_filter)*tile[1]
+        print Nframes
+        frames=[]
+        colors=[]
+        # show results of each filter
+        for i,filterid in enumerate(filter_show):
+            shot_ids=np.where(sel.shots_pass==filter_ids[filterid])[0]
+            np.random.shuffle(shot_ids)
+            for sid in shot_ids:
+                s=self.shots[sid]
+                if s is None:
+                    continue
+                for frameid in range(0,len(s),len(s)/frames_per_shot):
+                    frames.append((sid,frameid))
+                    if len(frames)==Nframes[i]: break
+                if len(frames)==Nframes[i]: break
+            # pad
+            if len(frames)<Nframes[i]:
+                npad=(Nframes[i]-len(frames))
+                print "Pad %d, filter id = %s, npad=%d"%(i,filterid,npad)
+                frames.extend([None]*npad)
+            # save colors
+            colors.append((len(frames),filter_colors[filterid]))
+        self.showgrid(frames,tile,colors)
+        #self.showgrid(frames,tile,None)
+        #return frames
 
     def filter_shotlength(self):
         nogo=0
@@ -151,7 +243,6 @@ class Selector:
                 passed+=1
         print "%s - Filter on shotlength: %.1f pct of shots passed (%d/%d, nogo=%d)"%(self.vidid, 100.0*passed/(passed+failed), passed,passed+failed, nogo)
 
-
     def filter_brightness(self):
         self.loadimages() # load images if not laoded yet
         self.calcbrightness()
@@ -163,12 +254,12 @@ class Selector:
             if not self.shots_pass[shotid]==1:
                 nogo+=1
                 continue
-            if np.min(self.brightness[shotid])<MIN_BRIGHTNESS:
+            if np.mean(self.brightness[shotid])<MIN_BRIGHTNESS:
                 toodark+=1
-                self.shots_pass[shotid]=filter_ids['bright']
-            elif np.max(self.brightness[shotid])>MAX_BRIGHTNESS:
-                toobright+=1
                 self.shots_pass[shotid]=filter_ids['dark']
+            elif np.mean(self.brightness[shotid])>MAX_BRIGHTNESS:
+                toobright+=1
+                self.shots_pass[shotid]=filter_ids['bright']
             else:
                 passed+=1
         print "%s - Filter on brightness: %.1f pct of shots passed (%d/%d), %.1f too bright, %.1f too dark"%\
@@ -187,9 +278,9 @@ class Selector:
             if np.mean(self.reldiffs[shotid])<MIN_REL_DIFF_FRAMES:
                 toostatic+=1
                 self.shots_pass[shotid]=filter_ids['static']
-            elif np.mean(self.reldiffs[shotid])>MAX_REL_DIFF_FRAMES:
-                toodynamic+=1
-                self.shots_pass[shotid]=filter_ids['dynamic']
+            #elif np.mean(self.reldiffs[shotid])>MAX_REL_DIFF_FRAMES:
+                #toodynamic+=1
+                #self.shots_pass[shotid]=filter_ids['dynamic']
             else:
                 passed+=1
         Nproc=self.Nshots-nogo
@@ -213,9 +304,17 @@ class Selector:
             return
         self.brightness=[None]*self.Nshots
         # TODO weigh with grayscale-perception weights
-        for shotid in np.where(self.shots_pass == 1)[0]:
+        for shotid in xrange(len(self.shots)):
             shot=self.shots[shotid] # np array with shape (shotlen, x,y,3)
+            if shot is None: continue
             self.brightness[shotid]=shot.reshape((shot.shape[0],-1)).mean(axis=1)
+        self.brightnessmean=[]
+        for ar in self.brightness:
+            if ar is None:
+                self.brightnessmean.append(None)
+            else:
+                self.brightnessmean.append(np.mean(ar))
+        self.brightnessmean=np.array(self.brightnessmean)
 
     def calcrelativediff(self):
         ## Calculate the difference between two frames and reweigh by the maximum of their brightness
@@ -234,6 +333,9 @@ class Selector:
         """ This is the core function that will apply all cheap filters and
         save the results in internal array self.shots_pass"""
         print "%s - Apply filters"%(self.vidid)
+        # clean shots_pass
+        self.shots_pass=np.ones((self.Nshots))
+        # apply filters
         self.filter_shotlength()
         self.loadimages()
         self.filter_brightness()
@@ -283,6 +385,14 @@ if __name__=="__main__" and len(sys.argv)==2:
     if not sel.filtered:
         sel.apply_filters_write()
         sel.dumpfiltered()
+    ## VISUALIZE
+    sel.show_filter_sample(['pass','bright','dark','static'])
+    # check darks manually
+    darks=np.where(sel.shots_pass==filter_ids['dark'])[0]
+    inds={}
+    for d in darks[:16]:
+        inds[d]=range(len(sel.shots[d]))
+    sel.showshots(inds)
 
     #frpath='samplevids/jF5eDmDPUDk/'
     #sel=Selector(frpath)
---
+++ util.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+from matplotlib.patches import Rectangle
+
+# Helper to draw colored background.
+# See http://stackoverflow.com/questions/14712665/matplotlib-subplot-background-axes-face-labels-colour-or-figure-axes-coor
+def full_extent(ax, pad=0.0):
+    """Get the full extent of an axes, including axes labels, tick labels, and
+    titles."""
+    # For text objects, we need to draw the figure first, otherwise the extents
+    # are undefined.
+    ax.figure.canvas.draw()
+    items = ax.get_xticklabels() + ax.get_yticklabels()
+#    items += [ax, ax.title, ax.xaxis.label, ax.yaxis.label]
+    items += [ax, ax.title]
+    bbox = Bbox.union([item.get_window_extent() for item in items])
+    return bbox.expanded(1.0 + pad, 1.0 + pad)
+
