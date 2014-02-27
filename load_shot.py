e90edec8fbd499376425685e954471752f542a2f
Tom Sercu <tom.sercu@gmail.com>
2014-02-27 16:50:23 -0500

    Give subject and filename, return list of frames

--- load_shot.py
+++ load_shot.py
@@ -1,8 +1,28 @@
 import simplejson as json
 import conf
+import os
+from os.path import join
 import sys
 
-path = sys.argv[1]
-n = int(sys.argv[2])
+subj = sys.argv[1]
+vidid = sys.argv[2]
+n = int(sys.argv[3])
 
+thumb_info = json.load(open(join(conf.path.thumbs,subj,vidid,conf.fn.shots),'r'))
+frame_info = json.load(open(join(conf.path.frames,subj,vidid,conf.fn.shots),'r'))
 
+assert(len(thumb_info) == len(frame_info))
+Nshots = len(thumb_info)
+assert(n < Nshots)
+
+# See if frame passed selection
+if frame_info[n] == [None, None]:
+    print -1
+else:
+    # print the full frame fn's
+    for i in xrange(frame_info[n][0], frame_info[n][1]+1):
+        print ((join(conf.path.frames,subj,vidid,conf.frames.fn) % i).format(vidid=vidid))
+    # or print the thumb fn's
+    #for thumb in thumb_info[n]:
+        #print ((join(conf.path.thumbs,subj,vidid,conf.thumbs.fn) )\
+                #.format(vidid=vidid,num='%06d'%thumb['thumb_n']))
